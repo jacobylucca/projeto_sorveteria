77860dfee8674ab6e198774035b74816d29afd73
Lucca Jacoby <jacobylucca@outlook.com>
2026-03-15 00:05:53 -0300

    Update SorveteService.java
    refactor: move business logic from Controller to Service layer

--- src/main/java/com/sorveteria/Sorvete/SorveteService.java
+++ src/main/java/com/sorveteria/Sorvete/SorveteService.java
@@ -1,4 +1,37 @@
 package com.sorveteria.Sorvete;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class SorveteService {
+
+    private final SorveteRepository sorveteRepository;
+
+    public List<Sorvete> listar() {
+        return sorveteRepository.findAll();
+    }
+
+    public Sorvete salvar(Sorvete sorvete) {
+        return sorveteRepository.save(sorvete);
+    }
+
+    public void excluir(Long id) {
+        sorveteRepository.deleteById(id);
+    }
+
+    public Sorvete atualizar(Long id, Sorvete sorveteAtualizado) {
+        return sorveteRepository.findById(id)
+                .map(sorvete -> {
+                    sorvete.setSaborSorvete(sorveteAtualizado.getSaborSorvete());
+                    sorvete.setMarcaSorvete(sorveteAtualizado.getMarcaSorvete());
+                    sorvete.setEstoqueAtual(sorveteAtualizado.getEstoqueAtual());
+                    sorvete.setDataValidade(sorveteAtualizado.getDataValidade());
+                    sorvete.setAtivo(sorveteAtualizado.getAtivo());
+                    return sorveteRepository.save(sorvete);
+                }).orElseThrow(() -> new RuntimeException("Não foi possível realizar a alteração. Tente novamente."));
+    }
 }
